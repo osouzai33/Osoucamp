e7e1c5c3d60e3cae45e0b63d7f5a0697c8aa781f
山崎惣大 <souta.y61233@gmail.com>
2024-12-18 00:55:10 +0900

    ⚡️ Add some feature
    CRUDを作成しました

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const randomCityIndex = Math.floor(Math.random() * cities.length);
+    const price = Math.floor(Math.random() * 5000) + 3000;
     const campground = new Campground({
       location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
       title: `${sample(descriptors)}・${sample(places)}`,
+      image: "https://picsum.photos/250/200 ",
+      description:
+        "キャンプとは、自然に身を委ねることである。世間に揉まれている日常から自分を解き放つ場である。そんなひと時をぜひここで。",
+      price,
     });
     await campground.save();
   }
